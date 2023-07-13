33527dfc0a39f79c86e845bbe8fdc4173af57c9e
ThePython <87204246+ThePython10110@users.noreply.github.com>
2023-07-13 13:37:10 -0700

    3.1

--- why_init/init.lua
+++ why_init/init.lua
@@ -1,3 +1,33 @@
 why = {}
 
 why.mineclone = minetest.get_modpath("mcl_core")
+
+local mod_list = {
+    "fake_liquids",
+    "get_group_items",
+    "lava_sponge",
+    "meat_blocks",
+    "slime_things",
+    "small_why_things",
+    "sound_machine",
+    "sticky_things",
+    "useless_beans",
+    "why_init",
+}
+
+if why.mineclone then
+    for _, mod in ipairs(mod_list) do
+        mcl_item_id.set_mod_namespace(mod, "why")
+    end
+    mcl_item_id.set_mod_namespace("ghost_blocks")
+else
+    minetest.register_on_mods_loaded(function()
+        for name, def in pairs(minetest.registered_items) do
+            for _, mod in ipairs(mod_list) do
+                if name:sub(1,#mod) == mod then
+                    minetest.register_alias("why:"..name:sub(#mod+2, -1), name)
+                end
+            end
+        end
+    end)
+end
